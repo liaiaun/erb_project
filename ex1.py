f887edfd36be3a3c643f0caa9e078831bb092a61
liaiaun <43697856+liaiaun@users.noreply.github.com>
2024-04-29 14:41:56 +0800

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,4 +1,35 @@
 # 创建一个学生数据表的程序,包括firtname,lastname,city,score,存储在dirctory中,然后存储在db中
+
+
+# 连接mysql 数据库  mysql -u root -p 123456  #连接数据库
+import mysql.connector
+
+def connect_to_database():
+    try:
+        # 连接到 MySQL 数据库
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="OliviaWqq",
+            password="123456",
+            database="user_info_db"
+        )
+        print("成功连接到数据库")
+        return conn
+    except mysql.connector.Error as error:
+        print("连接数据库时发生错误:", error)
+        return None
+
+def execute (conn, sql): #执行sql语句
+    try:
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        conn.commit()
+        print("执行成功")
+    except mysql.connector.Error as error:
+        print("执行SQL语句时发生错误:", error)
+        conn.rollback() #回滚
+
+
 def get_user_info(user_info_list: list):
     while True:
         student_info = input("请输入学生的信息（用逗号分隔，顺序为：firstname,lastname,city,score）：")
@@ -36,6 +67,14 @@ def main():
         print(usr_info)
 
     # todo:存储到db中
+    conn = connect_to_database()
+    if conn is not None:
+        for usr_info in usr_infos:
+            sql = "INSERT INTO user_info_db.user_info (firstname, lastname, city, score) VALUES (%s, %s, %s, %s)"
+            values = (usr_info['firstname'], usr_info['lastname'], usr_info['city'], usr_info['score'])
+            execute(conn, sql, values)
+        conn.close()
+
 
 if __name__ == '__main__':
     main()
